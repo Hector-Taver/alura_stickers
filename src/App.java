bd9bfa146994fcfc2ad4465efd79319bac5bdf36
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-28 16:49:51 -0300

    initial commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // Fazer uma conexão HTTP com a API e buscar os top 250 filmes
+
+        // Extrair só os dados que interessam (título, poster, classificação)
+
+        // Exibir e manipular os dados
+    }
+}
