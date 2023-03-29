829c79523b3e427c0084d1a2076d96c0e2a6d6e6
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-29 11:27:51 -0300

    business rules of the StickerGenerator class

---
+++ src/StickerGenerator.java
@@ -0,0 +1,13 @@
+public class StickerGenerator {
+  public void create() {
+    // Leitura da imagem
+
+    // Criar nova imagem em memória com transparência e com tamanho novo
+
+    // Copiar a imagem original para nova imagem (em memória)
+
+    // Escrever uma frase na nova imagem
+
+    // Escrever a nova imagem em um novo arquivo
+  }
+}
