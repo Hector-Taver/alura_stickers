4aa680b5c604276403e07adb8b8f7e8370dd7b36
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-29 17:41:03 -0300

    create Content class and Client class

--- src/App.java
+++ src/App.java
@@ -1,10 +1,5 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
@@ -12,31 +7,30 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Fazer uma conexão HTTP com a API e buscar os top 250 filmes
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI apiAddress = URI.create(url);
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(apiAddress).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+        Client http = new Client();
+        String json = http.findData(url);
 
         // Extrair só os dados que interessam (título, poster, classificação)
         JsonParser parser = new JsonParser();
-        List<Map<String, String>> moviesList = parser.parse(body);
+        List<Map<String, String>> contentList = parser.parse(json);
 
         // Exibir e manipular os dados
         StickerGenerator generator = new StickerGenerator();
-        for (Map<String, String> movie : moviesList) {
-            String urlImage = movie.get("image");
-            String title = movie.get("title");
+        for (int i = 0; i < 10; i++) {
+            Map<String, String> content = contentList.get(i);
+
+            String urlImage = content.get("image").replaceAll("(@+)(.*).jpg$", "$1.jpg");
+            String title = content.get("title");
             InputStream inputStream = new URL(urlImage).openStream();
 
-            String fileName = title + ".png";
+            String fileName = "assets/output/" + title + ".png";
 
             generator.create(inputStream, fileName);
 
             System.out.println(title);
             System.out.println(urlImage);
-            System.out.println(movie.get("imDbRating"));
+            System.out.println(content.get("imDbRating"));
             System.out.println();
         }
     }
---
+++ src/Client.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class Client {
+  public String findData(String url) {
+    try {
+      URI apiAddress = URI.create(url);
+      HttpClient client = HttpClient.newHttpClient();
+      HttpRequest request = HttpRequest.newBuilder(apiAddress).GET().build();
+      HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+      String body = response.body();
+      return body;
+    } catch (IOException | InterruptedException ex) {
+      throw new RuntimeException(ex);
+    }
+
+  }
+}
