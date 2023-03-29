e04cef038b4d9aa665b58f339658fe326815a523
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-29 12:13:34 -0300

    write new image in a new file

--- src/StickerGenerator.java
+++ src/StickerGenerator.java
@@ -8,12 +8,12 @@ import javax.imageio.ImageIO;
 public class StickerGenerator {
   public void create() throws IOException {
     // Leitura da imagem
-    BufferedImage originalImage = ImageIO.read(new File("assets/movie.jpg"));
+    BufferedImage originalImage = ImageIO.read(new File("assets/input/movie.jpg"));
 
     // Criar nova imagem em memória com transparência e com tamanho novo
     int width = originalImage.getWidth();
     int height = originalImage.getHeight();
-    int newHeight = height + 200;
+    int newHeight = (int) (height + (height * 0.2));
     BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
 
     // Copiar a imagem original para nova imagem (em memória)
@@ -23,5 +23,11 @@ public class StickerGenerator {
     // Escrever uma frase na nova imagem
 
     // Escrever a nova imagem em um novo arquivo
+    ImageIO.write(newImage, "png", new File("assets/output/sticker.png"));
+  }
+
+  public static void main(String[] args) throws IOException {
+    StickerGenerator generator = new StickerGenerator();
+    generator.create();
   }
 }
